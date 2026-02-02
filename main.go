9a420598b7129b623e3b4665f47071b3df433c22
Camille Onoda <info@camilleonoda.com>
2026-02-02 16:01:38 +0900

    fix: add input validation to prevent index out of bound panic on empty commands
    - Add length check for cleanedInput before accessing index 0
    - Create a config struct to keep track of the current pagination
    - Create the map command to move forward one page
    - Create the mapb command to move backward one page

--- main.go
+++ main.go
@@ -4,27 +4,50 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"time"
+
+	"github.com/CamilleOnoda/pokedexcli/internal/pokeapi"
 )
+
+type config struct {
+	pokeAPIClient       pokeapi.Client //talking to the PokeAPI client
+	nextLocationURL     *string        //where to go for the map command's next page
+	previousLocationURL *string        //where to go for the map command's previous page
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func(*config) error
+}
+
+var commands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	},
+	"map": {
+		name:        "map",
+		description: "Displays the names of 20 location areas in the Pokemon world",
+		callback:    commandMap,
+	},
+	"mapb": {
+		name:        "mapb",
+		description: "Displays the name of the previous 20 location areas in the Pokemon world",
+		callback:    commandMapb,
+	},
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	type cliCommand struct {
-		name        string
-		description string
-		callback    func() error
-	}
-
-	commands := map[string]cliCommand{
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback:    commandHelp,
-		},
+	cfg := config{
+		pokeAPIClient: pokeapi.NewClient(5 * time.Second),
 	}
 
 	for {
@@ -32,24 +55,65 @@ func main() {
 		scanner.Scan()
 		userInput := scanner.Text()
 		cleanedInput := cleanInput(userInput)
+		if len(cleanedInput) == 0 {
+			continue
+		}
 		value, ok := commands[cleanedInput[0]]
 		if !ok {
 			fmt.Print("Unknown command")
 		} else {
-			if err := value.callback(); err != nil {
+			if err := value.callback(&cfg); err != nil {
 				fmt.Printf("Error executing command: %v\n", err)
 			}
 		}
 	}
 }
 
-func commandExit() error {
+func commandExit(cfg *config) error {
 	fmt.Print("Closing the Pokedex... Goodbye!\n")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
-	fmt.Print("Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex\n")
+func commandHelp(cfg *config) error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message" +
+		"\nexit: Exit the Pokedex\n")
 	return nil
+}
+
+func commandMap(cfg *config) error {
+	locationsResp, err := cfg.pokeAPIClient.GetLocationAreas(cfg.nextLocationURL)
+	if err != nil {
+		return fmt.Errorf("Error fetching location areas: %w", err)
+	}
+
+	cfg.nextLocationURL = locationsResp.Next
+	cfg.previousLocationURL = locationsResp.Previous
+
+	for _, location := range locationsResp.Results {
+		fmt.Println(location.Name)
+	}
+
+	return nil
+}
+
+func commandMapb(cfg *config) error {
+	if cfg.previousLocationURL == nil {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+
+	locationsResp, err := cfg.pokeAPIClient.GetLocationAreas(cfg.previousLocationURL)
+	if err != nil {
+		return fmt.Errorf("Error fetching location areas: %w", err)
+	}
+	cfg.nextLocationURL = locationsResp.Next
+	cfg.previousLocationURL = locationsResp.Previous
+
+	for _, location := range locationsResp.Results {
+		fmt.Println(location.Name)
+	}
+
+	return nil
+
 }
