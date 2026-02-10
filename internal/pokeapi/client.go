2a6f6fca199aa50dcbcbfdfdff6e6b7cf66c4e08
Camille Onoda <info@camilleonoda.com>
2026-02-10 16:18:09 +0900

    refactor: define the PokeAPIClient interface

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,16 +3,24 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	pokecache "github.com/CamilleOnoda/pokedexcli/internal/pokecache"
 )
+
+type PokeAPIClient interface {
+	GetLocationAreas(pageURL *string) (LocationAreaResponse, error)
+}
 
 type Client struct {
 	httpClient *http.Client
+	cache      *pokecache.Cache
 }
 
-func NewClient(timeout time.Duration) Client {
-	return Client{
+func NewClient(timeout time.Duration) *Client {
+	return &Client{
 		httpClient: &http.Client{
 			Timeout: timeout,
 		},
+		cache: pokecache.NewCache(10 * time.Minute),
 	}
 }
