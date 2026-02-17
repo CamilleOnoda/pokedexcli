cbc6bb79b4bbe39fd25429bda5a7ceb9d5de443d
Camille Onoda <info@camilleonoda.com>
2026-02-17 17:14:03 +0900

    refactor: remove the http and caching logic to be handled in service.go

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -2,10 +2,6 @@ package pokeapi
 
 import (
 	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"time"
 )
 
 func (c *Client) GetLocationAreas(pageURL *string) (LocationAreaResponse, error) {
@@ -15,50 +11,16 @@ func (c *Client) GetLocationAreas(pageURL *string) (LocationAreaResponse, error)
 		url = *pageURL
 	}
 
-	cacheStart := time.Now()
-	cacheKey := url
-
-	if cachedData, found := c.cache.Get(cacheKey); found {
-		elapsed := time.Since(cacheStart)
-		fmt.Printf("CACHE HIT: retrieved data from cache in %v\n\n", elapsed)
-		var cachedResponse LocationAreaResponse
-		if err := json.Unmarshal(cachedData, &cachedResponse); err == nil {
-			return cachedResponse, nil
-		}
+	var svc Service = &ServiceImpl{client: *c}
+	data, err := svc.GetData(url)
+	if err != nil {
+		return LocationAreaResponse{}, err
 	}
 
 	var locationAreaResponse LocationAreaResponse
-
-	reqStart := time.Now()
-	resp, err := c.httpClient.Get(url)
-	if err != nil {
-		err := fmt.Errorf("Error making GET request to %s: %w", url, err)
+	if err := json.Unmarshal(data, &locationAreaResponse); err != nil {
 		return LocationAreaResponse{}, err
 	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		err := fmt.Errorf("Received a non-OK HTTP status code: %d", resp.StatusCode)
-		return LocationAreaResponse{}, err
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		err := fmt.Errorf("Error reading response body body: %w", err)
-		return LocationAreaResponse{}, err
-	}
-
-	c.cache.Add(cacheKey, body)
-
-	if err := json.Unmarshal(body, &locationAreaResponse); err != nil {
-		err := fmt.Errorf("Error unmarshaling JSON response: %w", err)
-		return LocationAreaResponse{}, err
-	}
-
-	elapsed := time.Since(reqStart)
-	fmt.Printf("API CALL: fetched data from API in %v\n\n", elapsed)
-
 	return locationAreaResponse, nil
 
 }
