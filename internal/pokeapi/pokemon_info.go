88933d39b122febe9da766a167a5978f6297f650
Camille Onoda <info@camilleonoda.com>
2026-02-17 17:13:23 +0900

    refactor: remove the http and caching logic to be handled in service.go

--- internal/pokeapi/pokemon_info.go
+++ internal/pokeapi/pokemon_info.go
@@ -3,36 +3,20 @@ package pokeapi
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 )
 
 func (c *Client) GetPokemonInfo(pokemonName string) (Pokemon, error) {
 	const baseURL = "https://pokeapi.co/api/v2"
 	url := baseURL + "/pokemon/" + pokemonName + "/"
 
+	var svc Service = &ServiceImpl{client: *c}
+	data, err := svc.GetData(url)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
 	var pokemon Pokemon
-
-	resp, err := c.httpClient.Get(url)
-	if err != nil {
-		err := fmt.Errorf("Error making GET request to %s: %w", url, err)
-		return Pokemon{}, err
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		err := fmt.Errorf("Received a non-OK HTTP status code: %d", resp.StatusCode)
-		return Pokemon{}, err
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		err := fmt.Errorf("Error reading response body: %w", err)
-		return Pokemon{}, err
-	}
-
-	if err := json.Unmarshal(body, &pokemon); err != nil {
+	if err := json.Unmarshal(data, &pokemon); err != nil {
 		err := fmt.Errorf("Error unmarshaling JSON response: %w", err)
 		return Pokemon{}, err
 	}
