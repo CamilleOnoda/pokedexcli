243a94577cf5ece16b8a57a3e27dc1d9ead71fb2
Camille Onoda <info@camilleonoda.com>
2026-02-17 17:12:43 +0900

    refactor: remove the http and caching logic to be handled in service.go

--- internal/pokeapi/pokemon_list.go
+++ internal/pokeapi/pokemon_list.go
@@ -3,9 +3,6 @@ package pokeapi
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"time"
 )
 
 func (c *Client) GetPokemonInLocationArea(pageURL *string) (PokemonInLocationResponse, error) {
@@ -15,53 +12,18 @@ func (c *Client) GetPokemonInLocationArea(pageURL *string) (PokemonInLocationRes
 		url = *pageURL
 	}
 
-	cacheStart := time.Now()
-	cacheKey := url
-	if cachedData, found := c.cache.Get(cacheKey); found {
-		elapsed := time.Since(cacheStart)
-		fmt.Printf("CACHE HIT: retrieved data from cache in %v\n", elapsed)
-		var cachedResponse PokemonInLocationResponse
-		if err := json.Unmarshal(cachedData, &cachedResponse); err == nil {
-			return cachedResponse, nil
-		}
+	var svc Service = &ServiceImpl{client: *c}
+	data, err := svc.GetData(url)
+	if err != nil {
+		return PokemonInLocationResponse{}, err
 	}
 
 	var pokemonInLocationResponse PokemonInLocationResponse
 
-	reqStart := time.Now()
-	resp, err := c.httpClient.Get(url)
-	if err != nil {
-		err := fmt.Errorf("Error making GET request to %s: %w", url, err)
-		return PokemonInLocationResponse{}, err
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		err := fmt.Errorf("Received a non-OK HTTP status code: %d", resp.StatusCode)
-		return PokemonInLocationResponse{}, err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		err := fmt.Errorf("Received a non-OK HTTP status code: %d", resp.StatusCode)
-		return PokemonInLocationResponse{}, err
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		err := fmt.Errorf("Error reading response body: %w", err)
-		return PokemonInLocationResponse{}, err
-	}
-
-	c.cache.Add(cacheKey, body)
-
-	if err := json.Unmarshal(body, &pokemonInLocationResponse); err != nil {
+	if err := json.Unmarshal(data, &pokemonInLocationResponse); err != nil {
 		err := fmt.Errorf("Error unmarshaling JSON response: %w", err)
 		return PokemonInLocationResponse{}, err
 	}
-
-	elapsed := time.Since(reqStart)
-	fmt.Printf("API CALL: fetched data from API in %v\n", elapsed)
 
 	return pokemonInLocationResponse, nil
 }
