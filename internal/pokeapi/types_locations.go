62480b6c3bed5d19e1f5c954b0b7f5ff29178ed2
Camille Onoda <info@camilleonoda.com>
2026-02-25 13:31:03 +0900

    refactor: define a LocationArea struct for better testing readability

--- internal/pokeapi/types_locations.go
+++ internal/pokeapi/types_locations.go
@@ -1,11 +1,13 @@
 package pokeapi
 
+type LocationArea struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
 type LocationAreaResponse struct {
-	Count    int     `json:"count"`
-	Next     *string `json:"next"`
-	Previous *string `json:"previous"`
-	Results  []struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"results"`
+	Count    int            `json:"count"`
+	Next     *string        `json:"next"`
+	Previous *string        `json:"previous"`
+	Results  []LocationArea `json:"results"`
 }
